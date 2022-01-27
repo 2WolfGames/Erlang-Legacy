45ed19ab6b8ef14be92688e35dd16b8d739863a7
wilberquito <wbq.software@gmail.com>
2022-01-27 16:45:27 +0100

    Horizontal movement using character controller 2d

---
+++ Ajax-TheGame/Assets/Assets/Scripts/AjaxMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AjaxMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] CharacterController2D characterController;
+
+    [SerializeField] float runSpeed = 30f;
+
+    float horizontalMovement = 0f;
+
+    // called one per frame
+    private void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+    }
+
+
+    // Time.fixedDeltaTime: represents how long was the last time
+    // the function was called (period time)
+
+    // Multiply by Time.fixedDeltaTime will ensure that Ajax
+    // moves same amount no matter how often this function was called
+
+    // It make consistence with multiple platforms
+    private void FixedUpdate()
+    {
+        float xSpeed = horizontalMovement * runSpeed;
+        characterController.Move(xSpeed, 0f);
+    }
+}
---
+++ Ajax-TheGame/Assets/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    [Range(0, .2f)] [SerializeField] float movementSmothing = 0.05f;
+    Rigidbody2D rigidbody2d;
+    Vector2 velocity = Vector2.zero;
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    // xvel & yvel -> [0.0 - 1.0]
+    public void Move(float xvel, float yvel)
+    {
+        Vector2 v = new Vector2(xvel, yvel);
+        Vector2 cv = rigidbody2d.velocity;
+        rigidbody2d.velocity = Vector2.SmoothDamp(cv, v, ref velocity, movementSmothing); // modifies @velocity
+    }
+}
