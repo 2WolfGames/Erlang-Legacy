9887fe85ea2a982ca490929fb855eb91dce489b4
Wilber Quito <54361466+wilberquito@users.noreply.github.com>
2022-01-29 23:31:59 +0100

    Refactor Ajax movement
    Now Ajax has dynamic jump depending on input pressing.
    
    I've also modified charactercontroller2d logic, migrated to ajaxmovement script

--- Ajax-TheGame/Assets/Assets/Scripts/CharacterController2D.cs
+++ Ajax-TheGame/Assets/Assets/Scripts/CharacterController2D.cs
@@ -2,73 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+    This class is thought to be generic,
+    any character should use this class, it takes cares of
+        - character orientation
+    
+    In future it may handle other common character eassues
+*/
+
 public class CharacterController2D : MonoBehaviour
 {
-    [SerializeField] LayerMask groundLayerMask;
+    Rigidbody2D rb;
 
-    [SerializeField] Transform groundedDetectorCenter;
-
-    [SerializeField] Vector2 groundedDetectorDimentions;
-
-    [SerializeField] int xOrientation = 1;
-
-    Rigidbody2D rigidbody2d;
-
-
-    bool grounded = false;
+    [SerializeField] Vector2 orientation = Vector2.zero;
 
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        ListenOrientationChanges();
+        orientation = rb.velocity.normalized;
     }
 
     void FixedUpdate()
     {
-        grounded = false;
+        HandleCharacterHorientation();
+    }
 
-        Collider2D[] colliders = Physics2D.OverlapBoxAll(
-            groundedDetectorCenter.position,
-            groundedDetectorDimentions,
-            0,
-            groundLayerMask);
-
-        foreach (Collider2D c in colliders)
+    void HandleCharacterHorientation()
+    {
+        if (Mathf.Abs(orientation.x) > 0.001)
         {
-            if (c.gameObject != gameObject)
-            {
-                grounded = true;
-                break;
-            }
+            UpdateCharacterOrientation(orientation.x > Mathf.Epsilon ? 1 : -1);
         }
-        UpdateOrientation(xOrientation);
-    }
-
-    // This method updates current velocity state
-    // we only update velocity.y when abs(yval) > 0
-    // and main character is touching the ground
-    public void Move(float xvel, float yvel)
-    {
-        rigidbody2d.velocity = new Vector2(xvel, rigidbody2d.velocity.y);
-
-        if (Mathf.Abs(yvel) <= Mathf.Epsilon) return;
-
-        if (!IsGrounded()) return;
-
-        rigidbody2d.velocity = rigidbody2d.velocity + new Vector2(0, yvel);
-    }
-
-    public bool IsGrounded()
-    {
-        return grounded;
     }
 
     // -1 left, 1 right 
-    void UpdateOrientation(int orientation)
+    void UpdateCharacterOrientation(int orientation)
     {
         if (orientation != -1 && orientation != 1) return;
 
@@ -82,19 +54,4 @@ public class CharacterController2D : MonoBehaviour
         }
     }
 
-    // It should only update orientation
-    // when there is a movement in horizontal axis
-    void ListenOrientationChanges()
-    {
-        if (Mathf.Abs(rigidbody2d.velocity.x) > 0.001)
-        {
-            xOrientation = Mathf.RoundToInt(Mathf.Clamp01(rigidbody2d.velocity.x) * 2 - 1);
-        }
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(groundedDetectorCenter.position, groundedDetectorDimentions);
-    }
 }
