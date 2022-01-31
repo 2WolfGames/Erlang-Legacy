ba9b08d3cbdae4d09fbc1d0a2507b974aa1a2c04
wilberquito <wbq.software@gmail.com>
2022-01-31 16:22:18 +0100

    El enemigo es destruido y movido por el ataque de Ajax

---
+++ Ajax-TheGame/Assets/Assets/Scripts/AjaxAttack.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AjaxAttack : MonoBehaviour
+{
+    [SerializeField] float timeBtwAttack;
+
+    [SerializeField] Transform attackRef;
+
+    [SerializeField] Vector2 basicAttackDimentions;
+
+    [SerializeField] float basicAttackComputeDelay = 0.05f;
+
+    [SerializeField] LayerMask whatIsEnemy;
+
+    float _timeBtwAttack;
+
+    void Update()
+    {
+        if (_timeBtwAttack <= 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                StartCoroutine(
+                    ComputeLayersOnRange((Collider2D[] colliders) =>
+                    {
+                        Vector3 forward = transform.forward;
+                        foreach (Collider2D collider in colliders)
+                        {
+                            Enemy enemy = collider.GetComponent<Enemy>();
+                            if (enemy)
+                            {
+                                enemy.Harakiri(new Vector2(forward.z, 0));
+                            }
+                        }
+                    })
+                );
+            }
+            _timeBtwAttack -= Time.deltaTime;
+        }
+        else
+        {
+            _timeBtwAttack = timeBtwAttack;
+        }
+    }
+
+    IEnumerator ComputeLayersOnRange(System.Action<Collider2D[]> callback)
+    {
+        yield return new WaitForSeconds(basicAttackComputeDelay);
+        Collider2D[] enemies = Physics2D.OverlapBoxAll(attackRef.position, basicAttackDimentions, 0, whatIsEnemy);
+        callback(enemies);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireCube(attackRef.position, basicAttackDimentions);
+    }
+}
---
+++ Ajax-TheGame/Assets/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Harakiri(Vector2 orientation)
+    {
+        Debug.Log("Someone touched me...");
+        Vector3 force = new Vector2(10 * orientation.x, 0 * orientation.y);
+        Debug.Log(force);
+        rb.velocity = rb.velocity + new Vector2(force.x, force.y);
+        StartCoroutine(
+            OnHarakiri(() =>
+            {
+                Destroy(gameObject);
+            })
+        );
+    }
+
+    IEnumerator OnHarakiri(System.Action callback)
+    {
+        yield return new WaitForSeconds(0.5f);
+        callback();
+    }
+
+
+}
