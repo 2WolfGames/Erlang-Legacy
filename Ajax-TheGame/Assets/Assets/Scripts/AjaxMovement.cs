0201806f10ac13696ba4cc45c989d4e12dd25561
Judit Quintana <judit.quintana.massana@gmail.com>
2022-02-10 17:31:06 +0100

    Set up of all animations. Problem with landing

--- Ajax-TheGame/Assets/Assets/Scripts/AjaxMovement.cs
+++ Ajax-TheGame/Assets/Assets/Scripts/AjaxMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AjaxMovement : MonoBehaviour
 {
+
     [SerializeField] Vector2 feetDimentions;
 
     [SerializeField] LayerMask whatIsGround;
@@ -27,6 +28,7 @@ public class AjaxMovement : MonoBehaviour
     float jumpTimeCounter = 0.2f;
 
     bool isJumping = false;
+    bool hasJumped = false;
 
     bool dashing = false;
 
@@ -53,6 +55,13 @@ public class AjaxMovement : MonoBehaviour
         if (!dashing)
         {
             rb.velocity = new Vector2(xOrientation * speed, rb.velocity.y);
+            ajaxFX.SetRunFX(rb.velocity.x != 0);
+
+            if (rb.velocity.y == 0 && hasJumped)
+            {
+                ajaxFX.TriggerLandFX();
+                hasJumped = false;
+            }
         }
     }
 
@@ -82,7 +91,10 @@ public class AjaxMovement : MonoBehaviour
         if (Input.GetButtonUp("Jump"))
         {
             isJumping = false;
+            hasJumped = true;
+
         }
+
     }
 
     // direction only support { -1, 1 }, meaning { left, right }
@@ -100,6 +112,7 @@ public class AjaxMovement : MonoBehaviour
         Freeze();
         this.rb.gravityScale = 0;
         this.rb.AddForce(new Vector2(dashSpeed * direction, 0f), ForceMode2D.Impulse);
+        ajaxFX.TriggerDashFX();
         yield return new WaitForSeconds(dashDuration);
         Freeze();
         this.rb.gravityScale = gravityScale;
@@ -114,7 +127,7 @@ public class AjaxMovement : MonoBehaviour
 
     bool IsGrounded()
     {
-        float extra = 0.25f;
+        float extra = 0.1f;
         RaycastHit2D ray = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0, Vector2.down, extra, whatIsGround);
         bool grounded = ray.collider != null;
         Color rayColor = grounded ? Color.green : Color.red;
@@ -123,6 +136,8 @@ public class AjaxMovement : MonoBehaviour
         Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x, 0), Vector2.down * (boxCollider2D.bounds.extents.y + extra), rayColor);
         Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x, boxCollider2D.bounds.extents.y + extra), Vector2.right * (2 * boxCollider2D.bounds.extents.x), rayColor);
 
+        Debug.Log(grounded);
         return grounded;
     }
+
 }
