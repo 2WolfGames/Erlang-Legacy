07d45025532aad627e6a434581dbbd9b7902253a
wilberquito <wbq.software@gmail.com>
2022-02-15 21:35:17 +0100

    Playing with jumping areas

---
+++ Ajax-TheGame/Assets/Springboard.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Springboard : MonoBehaviour
+{
+    Animator animator;
+
+    HashSet<GameObject> memory = new HashSet<GameObject>();
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (memory.Contains(other.gameObject)) return;
+        Debug.Log("*");
+        memory.Add(other.gameObject);
+        animator.SetBool("EXPAND", true);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!memory.Contains(other.gameObject)) return;
+        Debug.Log("?");
+        memory.Remove(other.gameObject);
+        animator.SetBool("EXPAND", false);
+    }
+}
