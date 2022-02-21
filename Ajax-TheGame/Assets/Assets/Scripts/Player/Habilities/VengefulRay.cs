e581244af217cf3acd74226b98bc280a2f2c811e
wilberquito <wbq.software@gmail.com>
2022-02-21 16:58:56 +0100

    Vengeful ray start

---
+++ Ajax-TheGame/Assets/Assets/Scripts/Player/Habilities/VengefulRay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VengefulRay : MonoBehaviour
+{
+    [SerializeField] float time;
+
+    [SerializeField] List<LayerMask> targets;
+
+    HashSet<GameObject> hitted = new HashSet<GameObject>();
+
+}
