1ec65701e68f02480c7864a7e185515911060d9e
wilberquito <wbq.software@gmail.com>
2022-02-21 16:41:55 +0100

    Review

--- Ajax-TheGame/Assets/Assets/Scripts/Environment/Engine.cs
+++ Ajax-TheGame/Assets/Assets/Scripts/Environment/Engine.cs
@@ -7,26 +7,29 @@ public class Engine : MonoBehaviour
 
     [SerializeField] Sprite activatedSprite;
 
-    [SerializeField] MechanismBase  mechanism;
+    [SerializeField] MechanismBase mechanism;
 
     [SerializeField] Transform myPoint;
 
     [SerializeField] Engine otherEngine;
+
     SpriteRenderer spriteRenderer;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        myPoint.position = new Vector3(myPoint.position.x,myPoint.position.y,0);
+        myPoint.position = new Vector3(myPoint.position.x, myPoint.position.y, 0);
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = defaultSprite;
     }
 
     //pre: -
     //post: activate mechanism if collider is the player Changes sprite too
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (mechanism.CanMechanismActivate(myPoint) && other.gameObject.tag == "Player"){
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (mechanism.CanMechanismActivate(myPoint) && other.gameObject.tag == "Player")
+        {
             mechanism.ActivateMechanism(myPoint);
             ChangeSprite(true);
         }
@@ -34,14 +37,19 @@ public class Engine : MonoBehaviour
 
     //pre: - 
     //post: changes sprite if it required and if its manager calls the same method of the other engine
-    public void ChangeSprite(bool manager){        
-        if (spriteRenderer.sprite != activatedSprite){
+    public void ChangeSprite(bool manager)
+    {
+        if (spriteRenderer.sprite != activatedSprite)
+        {
             spriteRenderer.sprite = activatedSprite;
-        } else {
+        }
+        else
+        {
             spriteRenderer.sprite = defaultSprite;
         }
 
-        if (manager && otherEngine){
+        if (manager && otherEngine)
+        {
             otherEngine.ChangeSprite(false);
         }
     }
---
+++ Ajax-TheGame/REVIEW.md
@@ -0,0 +1,11 @@
+# Review pull request #65
+
+- Door.cs & Elevator.cs are empty files, what's their's porpouse?
+
+- Engine concept should be change for lever
+
+- Door mechanism is not working
+
+- When you activate one lever, the other one does not change it's state
+
+- Could be nice that instead of trigger by collider could be trigger by pressing a key
