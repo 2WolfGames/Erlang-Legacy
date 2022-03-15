e77dad644bff28a28e4d6e4049d2ed125ad359d1
Judit Quintana <judit.quintana.massana@gmail.com>
2022-03-15 16:08:34 +0100

    trying another type of lifebar implementation

---
+++ Ajax-TheGame/Assets/Assets/Scripts/HealthBar/LifeBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeBar : MonoBehaviour
+{
+    //// Const values ////
+    const int cMaxLifeContainers = 9;
+    const float cCoroutineShakeWait = 1.25f;
+    const float cCoroutineFillLifeWait = 1f;
+    const float cCoroutineReflectLifeWait = 0.35f;
+
+    //// Serialized variables ////
+    [SerializeField] GameObject lifePrefab;
+    [SerializeField] GameObject emptyLifePrefab;
+    [SerializeField] List<LifeContainer> lifeContainers;
+    [SerializeField] ParticleSystem newLifeApearingParticleEffect;
+    
+    ////Global variables ////
+    int totalLifes;
+    int currentLifes;
+    Queue<int> pendentChanges;
+    bool modifying;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
