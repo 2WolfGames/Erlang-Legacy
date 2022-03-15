f4615f96bf0ff16b6249292ae1a5e3f1b000fa3c
wilberquito <wbq.software@gmail.com>
2022-03-15 18:05:55 +0100

    Compute collision orientation to trigger correct animation

--- Ajax-TheGame/Assets/Assets/Scripts/Player/Utils.cs
+++ Ajax-TheGame/Assets/Assets/Scripts/Player/Utils.cs
@@ -1,19 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils.Enums;
 
-public class Utils
+namespace Utils
 {
-    public enum Input
+    public class Functions
     {
-        LEFT,
-        NONE,
-        RIGHT
+        // pre: --
+        // post: compute if collision is made from front or backs
+        //      if collision is made in center, returns back and front randomly
+        public static CollisionSide ComputeCollisionSide(Transform origin, Transform other)
+        {
+            Vector3 direction = origin.InverseTransformPoint(other.position);
+            if (direction.x < 0)
+            {
+                return Enums.CollisionSide.BACK;
+            }
+            else if (direction.x > 0)
+            {
+                return Enums.CollisionSide.FRONT;
+            }
+            return Random.Range(0, 2) == 0 ? Enums.CollisionSide.BACK : Enums.CollisionSide.FRONT;
+        }
+    }
+    namespace Enums
+    {
+        public enum Input
+        {
+            LEFT,
+            NONE,
+            RIGHT
+        }
+
+        public enum Facing
+        {
+            LEFT,
+            RIGHT
+        }
+
+        public enum CollisionSide
+        {
+            FRONT,
+            BACK
+        }
     }
 
-    public enum Facing
-    {
-        LEFT,
-        RIGHT
-    }
 }
