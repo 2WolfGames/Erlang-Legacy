c8e8998bbbbaa9353139fd3e3ef1ce3a021a8c84
wilberquito <wbq.software@gmail.com>
2022-03-17 18:56:34 +0100

    Creating first IA enemy

--- Ajax-TheGame/Assets/Assets/Scripts/Behavior/RotateTaks.cs
+++ Ajax-TheGame/Assets/Assets/Scripts/Behavior/RotateTaks.cs
@@ -5,20 +5,17 @@ using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 
-namespace Behavior
+public class RotateTaks : Action
 {
-    public class RotateTaks : Action
+    [SerializeField] float speed = 0.1f;
+
+    public override TaskStatus OnUpdate()
     {
-        [SerializeField] float speed = 0.1f;
+        Debug.Log("On update");
 
-        public override TaskStatus OnUpdate()
-        {
-            Debug.Log("On update");
-
-            transform.RotateAround(transform.position, Vector3.up, speed * Time.deltaTime);
-            return TaskStatus.Running;
-        }
+        transform.RotateAround(transform.position, Vector3.up, speed * Time.deltaTime);
+        return TaskStatus.Running;
     }
-
 }
+
 
