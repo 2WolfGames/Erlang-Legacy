6f816391bdd494e798242289f534fe59ce078cad
wilberquito <wbq.software@gmail.com>
2022-03-17 16:21:16 +0100

    Adds behavior tree

---
+++ Ajax-TheGame/Assets/Assets/Scripts/Behavior/RotateTaks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+namespace Behavior
+{
+    public class RotateTaks : Action
+    {
+        [SerializeField] float speed = 0.1f;
+
+        public override TaskStatus OnUpdate()
+        {
+            Debug.Log("On update");
+
+            transform.RotateAround(transform.position, Vector3.up, speed * Time.deltaTime);
+            return TaskStatus.Running;
+        }
+    }
+
+}
+
