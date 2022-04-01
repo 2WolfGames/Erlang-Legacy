c391eda18799d2fcbe28b1bd20820a7d49f28732
Judit Quintana <judit.quintana.massana@gmail.com>
2022-04-01 22:33:11 +0200

    menus implemented

---
+++ Ajax-TheGame/Assets/Scripts/UI/InGameMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+using UnityEngine.UI;
+
+using Core.Shared;
+
+public class InGameMenu : MonoBehaviour
+{
+    public SkeletonGraphic skeletonGraphic;
+
+
+    private void Start() {
+        skeletonGraphic.AnimationState.SetAnimation(1,"init",false);
+    }
+
+    public void OnResumeHoverIn(){
+        skeletonGraphic.AnimationState.SetAnimation(1,"hoverInResume",false);
+    }
+
+    public void OnResumeHoverOut() {
+        skeletonGraphic.AnimationState.SetAnimation(1,"hoverOutResume",false);
+    }
+
+    public void OnSettingsHoverIn(){
+        skeletonGraphic.AnimationState.SetAnimation(1,"hoverInSettings",false);
+    }
+
+    public void OnSettingsHoverOut() {
+        skeletonGraphic.AnimationState.SetAnimation(1,"hoverOutSettings",false);
+    }
+
+    public void OnQuitHoverIn(){
+        skeletonGraphic.AnimationState.SetAnimation(1,"hoverInQuit",false);
+    }
+
+    public void OnQuitHoverOut() {
+        skeletonGraphic.AnimationState.SetAnimation(1,"hoverOutQuit",false);
+    }
+}
---
+++ Ajax-TheGame/Assets/Scripts/UI/LoadingMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+using UnityEngine.SceneManagement;
+
+public class LoadingMenu : MonoBehaviour
+{
+    [SerializeField] SkeletonGraphic skeletonGraphic;
+    [SerializeField] CanvasGroup canvasGroup;
+    AsyncOperation loadingOperation;
+
+    void Start()
+    {
+        canvasGroup.alpha = 0;
+        skeletonGraphic.AnimationState.SetAnimation(1,"animation",true);
+        StartCoroutine(FadeLoadingScreen(2));
+        StartCoroutine(nextScene("lvl1"));
+        
+    }
+
+    private void Update() {
+        if (loadingOperation != null){
+            if (loadingOperation.progress > 0.9f){
+                
+            }
+            Debug.Log(loadingOperation.progress);
+        }
+    } 
+
+    IEnumerator FadeLoadingScreen(float duration)
+    {
+        float startValue = canvasGroup.alpha;
+        float time = 0;
+        while (time < duration)
+        {
+            canvasGroup.alpha = Mathf.Lerp(startValue, 1, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        canvasGroup.alpha = 1;
+    }
+
+    IEnumerator nextScene(string sceneName){
+        yield return new WaitForSeconds(20);
+        loadingOperation = SceneManager.LoadSceneAsync(sceneName);
+    }
+
+}
