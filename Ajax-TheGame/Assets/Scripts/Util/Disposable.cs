57c56a2bee7bd3400ed1f0ac70f16cbd199a57c8
wilberquito <wbq.software@gmail.com>
2022-04-11 20:07:46 +0200

    feat: util components to handl effects, sounds and destroy objects

---
+++ Ajax-TheGame/Assets/Scripts/Util/Disposable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Core.Util
+{
+    // description:
+    //  component to link to object in order to make object destroy in `lifetime`
+    public class Disposable : MonoBehaviour
+    {
+        public float lifetime = 1.0f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Destroy(gameObject, lifetime);
+        }
+    }
+}
