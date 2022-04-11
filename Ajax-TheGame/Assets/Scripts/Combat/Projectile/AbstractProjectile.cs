1dd7141a6f6aadc77df3b8a0ff91c481b0b35759
wilberquito <wbq.software@gmail.com>
2022-04-11 20:09:12 +0200

    feat: adds logic to projectiles components

---
+++ Ajax-TheGame/Assets/Scripts/Combat/Projectile/AbstractProjectile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Core.Character.Player;
+using Core.Util;
+using UnityEngine;
+
+namespace Core.Combat.Projectile
+{
+    public abstract class AbstractProjectile : MonoBehaviour
+    {
+        public float damage;
+        public ParticleSystem explosionEffect;
+        public AudioClip splatterSound;
+
+        public GameObject Shooter { get; set; }
+
+        protected Vector2 force;
+
+        public event Action<AbstractProjectile> OnProjectileDestroyed;
+
+        public abstract void SetForce(Vector2 force);
+
+        protected void DestroyProjectile()
+        {
+            OnProjectileDestroyed?.Invoke(this);
+
+            if (splatterSound != null)
+                SoundManager.Instance.PlaySoundAtLocation(splatterSound, transform.position, 0.75f);
+
+            EffectManager.Instance.PlayOneShot(explosionEffect, transform.position);
+
+            Destroy(gameObject);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            // Can't shoot yourself
+            if (collision.gameObject == Shooter)
+                return;
+
+            // Projectile hit player
+            var player = collision.GetComponent<BasePlayer>();
+            if (player != null)
+            {
+                Vector2 force = this.force.normalized;
+                player.Hurt((int)damage, collision.gameObject);
+            }
+
+            DestroyProjectile();
+        }
+    }
+}
---
+++ Ajax-TheGame/Assets/Scripts/Combat/Projectile/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Core.Combat.Projectile
+{
+    public class Projectile : AbstractProjectile
+    {
+        public override void SetForce(Vector2 force)
+        {
+            this.force = force;
+            GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+        }
+    }
+}
