6e1d490a067e7a31ed8fc6dac744b09252f77f4e
Judit Quintana <judit.quintana.massana@gmail.com>
2022-04-11 16:22:38 +0200

    making a scene manager - game controller

---
+++ Ajax-TheGame/Assets/Scripts/GameSessionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSessionController : MonoBehaviour
+{
+    Transform savePoint;
+    Transform currentPoint;
+
+    void Awake(){
+        int numGameSessionControllers = FindObjectsOfType<GameSessionController>().Length;
+        if (numGameSessionControllers > 1){
+            Destroy(gameObject);
+        } else{
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
