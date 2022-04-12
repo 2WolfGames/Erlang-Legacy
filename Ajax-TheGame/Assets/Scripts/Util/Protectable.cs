7813e0b7e2bf0be6f412cfcec4ec490bda3c9941
wilberquito <wbq.software@gmail.com>
2022-04-12 21:05:38 +0200

    perf (Protectable): expands Touchable logic, adds readibilty, avoids using coroutines

---
+++ Ajax-TheGame/Assets/Scripts/Util/Protectable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Core.Util
+{
+    // description:
+    //  manage state if action can be done over game object
+    public class Protectable : MonoBehaviour
+    {
+        [SerializeField] float hitProtectionDuration;
+
+        float hitProtectionTimer;
+
+        public bool CanBeHit => hitProtectionTimer <= 0;
+
+        public bool IsProtected => !CanBeHit;
+
+        public void Update()
+        {
+            if (hitProtectionTimer > 0)
+                hitProtectionTimer -= Time.deltaTime;
+        }
+
+        public void ResetProtection()
+        {
+            this.hitProtectionTimer = hitProtectionDuration;
+        }
+
+        public void ResetProtection(float duration)
+        {
+            this.hitProtectionTimer = duration;
+        }
+    }
+}
+
