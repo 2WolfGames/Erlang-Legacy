40038ed8d476f3b0744f09cc25eb737f76d5ef8a
wilberquito <wbq.software@gmail.com>
2022-04-24 22:55:07 +0200

    feat(Combat): check if IA stills alive with conditional

---
+++ Ajax-TheGame/Assets/Scripts/Combat/IA/Condtional/IsDestroyed.cs
@@ -0,0 +1,14 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+
+
+namespace Core.Combat.IA.Conditional
+{
+    public class IsDestroyed : EnemyConditional
+    {
+        public override TaskStatus OnUpdate()
+        {
+            bool destroyed = destroyable.IsDestroyed;
+            return destroyed ? TaskStatus.Success : TaskStatus.Failure;
+        }
+    }
+}
