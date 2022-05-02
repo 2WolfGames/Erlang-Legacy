a503d40b899e8f32a96509846daa370668bfd358
wilberquito <wbq.software@gmail.com>
2022-05-02 16:16:10 +0200

    feat: camera controller to handle shake events

---
+++ Ajax-TheGame/Assets/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using Cinemachine;
+using Core.Player.Controller;
+using UnityEngine;
+
+public class PlayerAttacher : MonoBehaviour
+{
+    void Start()
+    {
+        var player = PlayerController.Instance;
+        var camera = GetComponent<CinemachineVirtualCamera>();
+        camera.Follow = player.transform;
+    }
+}
