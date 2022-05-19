2fa04c9765e18188db084617fc23439a8c0f79e2
Judit Quintana <judit.quintana.massana@gmail.com>
2022-05-19 17:01:39 +0200

    feat: dialogue prototype

---
+++ Erlang-Legacy/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] Transform TalkPoint;
+
+    private bool playerIn = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerIn) {
+            if (Input.GetKeyDown(KeyCode.S)){
+                Debug.Log("talk to npc");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player"){
+            playerIn = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player"){
+            playerIn = false;
+        }
+    }
+}
