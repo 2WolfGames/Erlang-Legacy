111e548cc410966eeb7d29c6b05843abac646f88
Judit Quintana Massana <61147274+Jumalita@users.noreply.github.com>
2022-05-26 16:30:37 +0200

    Talking system#128 (#132)
    * feat: add captain Java NPC to game
    
    * feat: dialogue prototype
    
    * feat: display characters name at dialogue
    
    * feat: dialogue transitions added
    
    * feat: Dialogue box behavior complete
    
    * feat: talking system done
    
    * feat: code commented & few things fixed
    
    * feat: prefab Captain Java and dialogue Canvas
    
    * fix: null reference exception resolved
    
    * perf: conditional
    
    Co-authored-by: wilberquito <wbq.software@gmail.com>

--- Erlang-Legacy/Assets/Scripts/GameSession/SceneEntrance.cs
+++ Erlang-Legacy/Assets/Scripts/GameSession/SceneEntrance.cs
@@ -1,7 +1,6 @@
 ﻿using Core.Player.Controller;
-using Core.Player.Util;
 using Core.Shared.Enum;
-using DG.Tweening;
+using Core.Shared;
 using UnityEngine;
 
 namespace Core.GameSession
@@ -24,44 +23,20 @@ namespace Core.GameSession
         //post: animates player entering the current scene
         public void MakeEntrance()
         {
-            OnStartEntrance();
             var player = PlayerController.Instance;
-            player.transform.DOMove(entrancePoint.position, entranceTime)
-                            .SetDelay(entranceWaitTime)
-                            .OnComplete(
-                                () =>
-                                {
-                                    OnEndEntrance();
-                                }
-                            );
-        }
-
-        //pre: PlayerController.Instance != null
-        //post: sets player stats for entrance
-        private void OnStartEntrance()
-        {
-            var player = PlayerController.Instance;
-            //facing player
-            PlayerFacing facing = spawnPoint.position.x - entrancePoint.position.x > 0
-            ? PlayerFacing.Left : PlayerFacing.Right;
-            player.SetFacing(facing);
             //positioning
             player.transform.position = spawnPoint.position;
-            //starting animation of runing
-            player.Animator.SetBool(CharacterAnimations.Running, true);
-            //while entrance, player controll disabled
+            //where to face player
+            PlayerFacing facing = spawnPoint.position.x - entrancePoint.position.x > 0
+            ? PlayerFacing.Left : PlayerFacing.Right;
+            //player not controllable 
             player.Controllable = false;
+
+            MovePlayer.Trigger(entrancePoint,entranceWaitTime,facing,entranceTime,() =>{
+                player.Controllable = true;
+            });
+
         }
 
-        //pre: PlayerController.Instance != null
-        //post: sets player stats for gameplay
-        private void OnEndEntrance()
-        {
-            var player = PlayerController.Instance;
-            //starting animation of runing
-            player.Animator.SetBool(CharacterAnimations.Running, false);
-            //entrance ended, player can move
-            player.Controllable = true;
-        }
     }
 }
---
+++ Erlang-Legacy/Assets/Scripts/NPC/Util/NPCData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Core.NPC.Util
+{
+    [System.Serializable]
+    public class NPCData
+    {
+        public string npcName;
+
+        [Tooltip("Phrases and NPCActions are connected, same lenght is requaired")]
+        [TextArea(3, 135)]
+        public string[] phrases;
+        
+        [Tooltip("Phrases and NPCActions are connected, same lenght is requaired")]
+        public NPCActions[] npcActions;
+        
+    }
+
+}
