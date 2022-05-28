f48f7d4b583bbdfcd0c8224b391a2b80119bdaed
wilberquito <wbq.software@gmail.com>
2022-05-28 15:02:00 +0200

    perf: on recover start only throw if player steal alive after taking live

--- Erlang-Legacy/Assets/Core/Player/Controller/PlayerController.cs
+++ Erlang-Legacy/Assets/Core/Player/Controller/PlayerController.cs
@@ -117,15 +117,17 @@ namespace Core.Player.Controller
             if (protectable.IsProtected)
                 return;
 
-            movementController.FreezeVelocity();
+            FreezeMovement();
+
             TakeLifes(damage);
 
             ComputeSideHurtAnimation(other.transform);
-            
+
             if (shakeCameraOnHurt)
                 CameraManager.Instance?.ShakeCamera();
 
-            OnRecoverStart(playerData.Health.HP > 0);
+            if (IsAlive())
+                OnRecoverStart();
         }
 
         private void TakeLifes(int damage)
@@ -144,21 +146,20 @@ namespace Core.Player.Controller
         }
 
         // pre: called at first key frame hit (backward & forward) animations       
-        private void OnRecoverStart(bool isAlive)
+        private void OnRecoverStart()
         {
-            if (protectable.IsProtected)
-                return;
-
+            Animator.SetBool(CharacterAnimations.Blink, true);
             inRecoverProcess = true;
             protectable.SetProtection(ProtectionType.INFINITE);
-            if(isAlive)
-                Animator.SetBool(CharacterAnimations.Blink, true);
             controllable = false;
         }
 
         // desc: to be called at end of recover animations with and event
         public void OnRecoverComplete()
         {
+            if (!inRecoverProcess)
+                return;
+
             controllable = true;
             StartCoroutine(AfterRecoverComplete());
         }
@@ -213,6 +214,11 @@ namespace Core.Player.Controller
         public void FreezeMovement()
         {
             movementController.FreezeVelocity();
+        }
+
+        public bool IsAlive()
+        {
+            return playerData.Health.HP > 0;
         }
 
     }
