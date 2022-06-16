5fde4c7b58c3c5577b3a92b5c050a689edbab14c
Judit Quintana <judit.quintana.massana@gmail.com>
2022-06-16 16:50:36 +0200

    feat: Tutorial logic done, few retoches needed

---
+++ Erlang-Legacy/Assets/KeyIndicatorDisposer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using Core.Shared.Enum;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class KeyIndicatorDisposer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI function;
+    [SerializeField] TextMeshProUGUI key;
+    CanvasGroup canvasGroup => GetComponentInChildren<CanvasGroup>();
+    //ButtonAnimation buttonAnimation => GetComponentInChildren<CanvasGroup>();
+
+    bool showing = false;
+    float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup.alpha = 0;
+    }
+
+    void Update() {
+        if (showing && Input.GetKeyDown(key.text.ToLower())){
+            HideTutorial();
+        }
+    }
+
+    public void ShowTutorial(GameKey gameKey, string functionallity){
+        function.text = functionallity;
+        key.text = gameKey.ToString().ToUpper();
+        showing = true;
+        canvasGroup.DOFade(1,0.25f);
+    }
+
+    public void HideTutorial(){
+        showing = false;
+        canvasGroup.DOFade(0,0.25f);
+    }
+}
---
+++ Erlang-Legacy/Assets/KeyIndicatorTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using Core.Shared.Enum;
+using UnityEngine;
+
+public class KeyIndicatorTrigger : MonoBehaviour
+{
+    [SerializeField] GameKey gameKey;
+    [SerializeField] string function; 
+    [SerializeField] float waitTimeBeforeShowingKeys;
+
+    private KeyIndicatorDisposer currentkeyIndicatorDisposer;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "KeyIndicatorDisposer"){
+            Debug.Log("1");
+            currentkeyIndicatorDisposer = other.gameObject.GetComponent<KeyIndicatorDisposer>(); 
+            StartCoroutine(ShowKeyIndication());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "KeyIndicatorDisposer" && currentkeyIndicatorDisposer != null){
+            currentkeyIndicatorDisposer = null;
+        }
+    }
+
+    IEnumerator ShowKeyIndication(){
+        yield return new WaitForSeconds(waitTimeBeforeShowingKeys);
+        currentkeyIndicatorDisposer?.ShowTutorial(gameKey,function);
+    }
+}
