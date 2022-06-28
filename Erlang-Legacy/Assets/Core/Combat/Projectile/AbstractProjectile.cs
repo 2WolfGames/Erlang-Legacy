8454f4d3881897de8b8a7de8a79df21f578de8e0
wilberquito <wbq.software@gmail.com>
2022-06-28 16:39:25 +0200

    feat: projectile supports anonymous functions, explosion and sound effect

--- Erlang-Legacy/Assets/Core/Combat/Projectile/AbstractProjectile.cs
+++ Erlang-Legacy/Assets/Core/Combat/Projectile/AbstractProjectile.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Core.Manager;
+using UnityEngine;
 
 
 namespace Core.Combat.Projectile
@@ -6,10 +7,23 @@ namespace Core.Combat.Projectile
     public abstract class AbstractProjectile : MonoBehaviour
     {
         public Vector2 force;
+        public ParticleSystem explosionEffect;
+        public AudioSource explosionSound;
+        public Collider2DEvent OnEnter, OnExit;
 
         public virtual void SetForce(Vector2 force)
         {
             this.force = force;
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (explosionEffect != null)
+                EffectManager.Instance?.PlayOneShot(explosionEffect, transform.position);
+            if (explosionSound != null)
+                explosionSound.Play();
+            if (OnEnter != null)
+                OnEnter.Invoke(other);
         }
     }
 }
