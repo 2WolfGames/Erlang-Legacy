67dabd15db154e38ec760fdb1e71e1275baee98a
Judit Quintana <judit.quintana.massana@gmail.com>
2022-07-03 23:25:03 +0200

    feat: Omed Island Zone 1 - base

--- Erlang-Legacy/Assets/Core/Shared/Enum/SceneID.cs
+++ Erlang-Legacy/Assets/Core/Shared/Enum/SceneID.cs
@@ -3,6 +3,7 @@ namespace Core.Shared.Enum
 {
     public enum SceneID
     {
-        StartMenu, LoadingScene, lvl1, FirstIsland
+        StartMenu, LoadingScene, lvl1, FirstIsland,
+        OmedIsland_Zone1, OmedIsland_Zone2
     }
 }
