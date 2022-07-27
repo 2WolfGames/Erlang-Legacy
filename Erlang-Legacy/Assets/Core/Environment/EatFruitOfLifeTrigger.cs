116b24db7faddedd2d1ce067b490213122cb28b2
wilberquito <wbq.software@gmail.com>
2022-07-27 16:47:33 +0200

    fix: health adder

--- Erlang-Legacy/Assets/Core/Environment/EatFruitOfLifeTrigger.cs
+++ Erlang-Legacy/Assets/Core/Environment/EatFruitOfLifeTrigger.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Core.Player.Utility;
 using Core.Player.Controller;
 using Core.ScriptableEffect;
@@ -13,7 +11,7 @@ namespace Core.Environment
         private bool playerIn = false;
         private bool processHasStarted = false;
         [SerializeField] GameObject onDestroyParticleEffect;
-        [SerializeField] HealthAdderEffect healthAdder;
+        [SerializeField] HealthAdder healthAdder;
 
         //pre: --
         //post: if player is in range and clicks, eat fruit to gain life process starts.
