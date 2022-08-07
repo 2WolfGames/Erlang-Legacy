cf8ae9cf4054873060b73ba4b0544e0024964987
Judit Quintana <judit.quintana.massana@gmail.com>
2022-08-07 13:06:36 +0200

    feat: ordering and commenting

---
+++ Erlang-Legacy/Assets/Core/Cinematographics/C_IslandTransition_controller.cs
@@ -0,0 +1,41 @@
+﻿using Core.Shared;
+using Core.Shared.Enum;
+using UnityEngine;
+using Core.NPC;
+
+namespace Core.Cinematographics
+{
+    public class C_IslandTransition_controller : MonoBehaviour
+    {
+        [SerializeField] DialogueManager captainJava_dialogueManager;
+        bool transitionStarted = false;
+
+        //pre: --
+        //post: on player colision starts island transition
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                StartIslandTransition();
+            }
+        }
+
+        //pre: --
+        //post: chacks if there is no transition started, if correct triggers Captain Java dialogue.
+        public void StartIslandTransition()
+        {
+            if (transitionStarted)
+                return;
+
+            transitionStarted = true;
+            captainJava_dialogueManager.TriggerDialogue();
+        }
+
+        //pre: --
+        //post: Unity Event for the end of Captain Java speach: loads new scene.
+        public void LoadNextScene()
+        {
+            StartCoroutine(Loader.LoadWithDelay(SceneID.MenIsland_Zone1, 0f));
+        }
+    }
+}
---
+++ Erlang-Legacy/Assets/Core/Cinematographics/EndGameSceneBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using Core.Shared.Enum;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using Core.Shared;
+using UnityEngine.UI;
+
+namespace Core.Cinematographics
+{
+    public class EndGameSceneBehaviour : MonoBehaviour
+    {
+        [SerializeField] GameObject ladybug;
+        [SerializeField] TextMeshProUGUI createdBy;
+        [SerializeField] GameObject creators;
+        [SerializeField] Image gameTitle;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ShowTitle();
+        }
+
+        //pre: --
+        //post: Shows  game title and calls ShowCredits()
+        private void ShowTitle()
+        {
+            gameTitle.DOFade(1, 5f).SetDelay(2f).OnComplete(() =>
+            {
+                ShowCredits();
+            });
+        }
+
+        //pre: --
+        //post: Shows credits and calls MoveBugs()
+        private void ShowCredits()
+        {
+            createdBy.rectTransform.DOLocalMoveY(200, 3f).OnComplete(() =>
+            {
+                creators.transform.DOLocalMoveY(15, 3f);
+                MoveBugs();
+            });
+        }
+
+        //pre: --
+        //post: Makes ladybug move through scene and starts fading all away
+        private void MoveBugs()
+        {
+            ladybug.transform.DOLocalMoveX(600, 10f).SetDelay(6f).OnComplete(() =>
+            {
+                FadeAllAndGoToStart();
+            });
+        }
+
+        //pre: --
+        //post: Fades all away and returns to start menu
+        private void FadeAllAndGoToStart()
+        {
+            GetComponent<CanvasGroup>().DOFade(0, 10f).SetDelay(5f).OnComplete(() =>
+            {
+                StartCoroutine(Loader.LoadWithDelay(SceneID.StartMenu, 0));
+            });
+        }
+
+    }
+}
